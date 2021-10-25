eb0abfb021135a3765d3207b8e48ff7240ea6ef0
Amber Orrange <orrangeamber@gmail.com>
2021-10-25 15:54:32 -0500

    initial commit

---
+++ prep exercise.py
@@ -0,0 +1,31 @@
+print("Hello world!")
+print()
+print("How are you?")
+#this is a comment this is a comment
+
+# print("you can also comment out blocks of code.....with command and forward slash /") 
+
+
+fav_word = "food"
+print(fav_word + "!!!" + " yum")
+
+int_ex = 100_000
+print(int_ex)
+
+sleepy = True   
+print(sleepy)
+
+
+
+def show_greeting():
+    print("Hello, world!")
+    print("Have a nice day.")
+
+
+
+color = input("what is your favorite color? ")
+
+if color == "red":
+    print("I like red too")
+if color == "pink":
+    print("pink is cool, I guess")
